d560cbd6a1c1784c8b33117b581eca0e8634c065
Dinis Morais <dinismorais2000@gmail.com>
2021-07-20 22:41:37 +0100

    Update database.js

--- src/model/database.js
+++ src/model/database.js
@@ -1,12 +1,17 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-'por1click',
-'postgres',
-'1234',
+'d410g3fj5360o1',
+'wjilpqmgptaske',
+'1e50e55305a99be6700829c171254c9a17b1c76bd69843023247fceee2bed360',
 {
-host: 'localhost',
+host: 'ec2-54-228-174-49.eu-west-1.compute.amazonaws.com',
 port: '5432',
-dialect: 'postgres'
+dialect: 'postgres',
+dialectOptions:{
+        ssl:{          
+            rejectUnauthorized: false        
+        }
+    },
 }
 );
 module.exports = sequelize;
